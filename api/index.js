a79ebbd6009702189a83510a243bf57373b6bbea
thosoya1973 <79416770+thosoya1973@users.noreply.github.com>
2021-06-23 23:01:58 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
-const { auth } = require("express-oauth2-bearer");
+const { auth, requiredScopes } = require("express-oauth2-bearer");
 // 👉 Replace this with express-oauth2-bearer require 👈
 const {
   checkUrl,
@@ -52,7 +52,7 @@ app.get("/total", (req, res) => {
 app.use(auth());
 // 👇 private routes below 👇
 
-app.get("/reports", (req, res) => {
+app.get("/reports", requiredScopes('read:reports'), (req, res) => {
   res.send(expenses);
 });
 
